168cee449234e13866f0b8d4529d952b30dc7ad2
Lizaveta <elis.liza567@gmail.com>
2020-06-30 21:22:45 +0300

    dashboard order page

--- components/purchase.php
+++ components/purchase.php
@@ -10,12 +10,16 @@
     <!-- img and desc -->
     <div class="main_desc">
       <img src="img/content/product-photo.png" alt="product">
+      <div class="count">
+        <p>1</p>
+      </div>
       <!-- information block -->
       <div class="information_block">
         <p class="brand_of_product">бренд</p>
         <p class="name_of_product">название продукта</p>
         <p class="size_of_product">Pазмер: <span class="size">M</span> </p>
         <p class="delivery">Доставка: <span> в наличии</span> </p>
+        <p class="sku">SKU: <span> 222</span> </p>
       </div>
     </div>
     <!-- category for dashboard -->
@@ -46,16 +50,17 @@
       <div class="quantity_final">
         <p>1</p>
       </div>
-      <!-- summ -->
-      <div class="summ_block">
-          <p class="price_of_product"> 450</p> <span>$</span>
-      </div>
       <!-- quantity and summ for checkout -->
       <div class="quantity_and_summ">
         <p>1</p>
         <span>x</span>
         <p> 450</p> <span>$</span>
       </div>
+      <!-- summ -->
+      <div class="summ_block">
+          <p class="price_of_product"> 450</p> <span>$</span>
+      </div>
+
       <!-- views for dashboard-->
       <div class="views">
         <p>150 просмотров</p>
---
+++ dashboard_order.php
@@ -0,0 +1,93 @@
+<?php include 'components/header.php'; ?>
+<main>
+  <div class="dashboard_page container">
+  <div class="grey_line"></div>
+   <!-- add goods page -->
+    <div class="dashboard_order">
+      <!-- sidebar -->
+      <div class="dashboard_sidebar">
+        <div class="name_of_web">
+          <p>Дашборд</p>
+        </div>
+          <ul class="dashboard_links">
+            <li> <a href="dashboard.php"><img src="img/icons/home.png" alt="home"> <span>Главная</span>  </a> </li>
+            <li> <a href="dashboard_orders.php"><img src="img/icons/shopping-basket_active.png" alt="basket"> <span class="active">Заказы</span>  </a> </li>
+            <li> <a href="dashboard_goods.php"><img  class="tag" src="img/icons/tag.png" alt="tag" > <span>Товары</span>  </a> </li>
+          </ul>
+        </div>
+        <!-- main -->
+        <div class="dashboard_order_block">
+          <div class="name_of_web">
+            <p>Заказ #342</p>
+            <span>21  мая, 2020 в 14:43</span>
+            <div class="filter_status">
+              <span class="orange"></span>
+              <p>В обработке</p>
+            </div>
+          </div>
+          <div class="orders_items">
+            <?php for($i = 0; $i < 4; $i++): ?>
+                   <?php include 'components/purchase.php' ?>
+            <?php endfor; ?>
+          </div>
+          <div class="payed">
+            <div class="name_of_web">
+              <p>Оплачено</p>
+            </div>
+            <div class="total">
+              <div class="row come_to ">
+                <span>Итого</span>
+                <span class="additional">10 товаров</span>
+                <span class="all_price count">450$</span>
+              </div>
+              <div class="row del ">
+                <span>Доставка</span>
+                <span class="additional">бесплатная доставка (1,1 кг)</span>
+                <span class="delivery_price count">0$</span>
+              </div>
+              <div class="row del ">
+                <span>Налог</span>
+                <span class="additional"></span>
+                <span class="tax count">0$</span>
+              </div>
+              <div class="row in_all ">
+                <p>Всего</p>
+                <span class="additional"></span>
+                <p class="total_price count">450$</p>
+              </div>
+            </div>
+          </div>
+          <!-- done button -->
+          <button class="default_button black_button">
+            <p>Выполнен</p>
+          </button>
+        </div>
+        <!-- person information -->
+        <div class="client_information">
+          <div class="name_of_web">
+            <p> Клиент</p>
+          </div>
+          <div class="client_main">
+            <span class="name_and_surname">Имя Фамилия</span>
+
+            <p class="subtitle">Контакты</p>
+            <a href = "mailto: abc@example.com"> email.com</a>
+            <a href="tel:+79959567575">+ 7 995 956 75 75</a>
+
+            <p class="subtitle">Доставка</p>
+            <span class="name_and_surname">Имя Фамилия</span>
+            <span class="email">email.com</span>
+            <span class="tel">+ 7 995 956 75 75</span>
+
+            <span class="address">Адрес,</span>
+            <span class="dir">Область,</span>
+            <span class="city">Город, почтовый индекс</span>
+
+            <p class="subtitle">Платёжный адрес</p>
+            <span>такой же</span>
+          </div>
+        </div>
+      </div>
+    </div>
+</main>
+ <?php include 'components/footer.php'; ?>
